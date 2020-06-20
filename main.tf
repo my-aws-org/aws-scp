031d83edfd6afaf89b874da03a0019b33737b9e7
Thiru <thiru@ntucenterprise.sg>
2020-06-21 02:10:08 +0800

    Initial Test

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ resource "aws_organizations_policy" "scp-bill-pay-protect" {
   content = file("polices/scp-bill-pay-protect.json")
  }
 
-#resource "aws_organizations_policy" "scp-iam-user-accesskey-disable" {
-#  name = "scp-iam-user-accesskey-disable"
-#  description = "This SCP restricts IAM principals from creating new IAM users or IAM Access Keys in an AWS account."
-#  content = file("polices/scp-iam-user-accesskey-disable.json")
-#}
+resource "aws_organizations_policy" "scp-iam-user-accesskey-disable" {
+  name = "scp-iam-user-accesskey-disable"
+  description = "This SCP restricts IAM principals from creating new IAM users or IAM Access Keys in an AWS account."
+  content = file("polices/scp-iam-user-accesskey-disable.json")
+}
 
 resource "aws_organizations_policy" "scp-s3-public-access-block" {
   name = "scp-s3-public-access-block"
